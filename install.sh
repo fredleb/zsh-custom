49dee9fb3f17beb1e1163191a8a8a2a93131f9a2
Frederic Lebouc <frederic_lebouc@yahoo.fr>
2017-08-16 18:07:11 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+now=`date +%Y%m%d%H%M%S`
+
+# Make backup copies from .zshrc
+cp $HOME/.zshrc $HOME/.zshrc.$now
+
+
+ANTIGEN_DIR=~/.antigen
+mkdir -p $ANTIGEN_DIR
+
+ANTIGEN=$ANTIGEN_DIR/antigen.zsh
+
+# download antigen
+if [ ! -f $ANTIGEN ]; then
+	curl -L git.io/antigen > $ANTIGEN
+fi
+
+# Replace ~/.zshrc with our version
+cp zshrc $HOME/.zshrc
+
